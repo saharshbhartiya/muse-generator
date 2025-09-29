44dcc3aa254189a2794064743768410f086be8fa
saharshbhartiya <saharshbhartiya@gmail.com>
2025-09-29 13:50:42 +0530

    Connection for DataBase

--- src/main/java/mozart/muse_generator/controller/PromptController.java
+++ src/main/java/mozart/muse_generator/controller/PromptController.java
@@ -1,10 +1,11 @@
 package mozart.muse_generator.controller;
 
 import mozart.muse_generator.dto.MusicPromptDto;
+import mozart.muse_generator.entity.SavedPrompt;
 import mozart.muse_generator.service.MusicPromptService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/prompts")
@@ -21,5 +22,14 @@ public class PromptController {
     @GetMapping("/chords")
     public MusicPromptDto getChordPrompt(){
         return musicPromptService.getChordProgression();
+    }
+
+    @PostMapping("/save")
+    public SavedPrompt savePrompt(@RequestBody MusicPromptDto promptToSave){
+        return musicPromptService.savedPrompt(promptToSave);
+    }
+    @GetMapping("/saved")
+    public List<SavedPrompt> getSavedPrompts(){
+        return musicPromptService.getAllSavedPrompts();
     }
 }
--- src/main/java/mozart/muse_generator/service/MusicPromptService.java
+++ src/main/java/mozart/muse_generator/service/MusicPromptService.java
@@ -1,6 +1,8 @@
 package mozart.muse_generator.service;
 
 import mozart.muse_generator.dto.MusicPromptDto;
+import mozart.muse_generator.entity.SavedPrompt;
+import mozart.muse_generator.repository.SavedPromptRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,11 +16,26 @@ public class MusicPromptService {
             "vi - IV - I - V  (The 'Sensitive Female' Progression)",
             "I - IV - V - I   (The 'Classic Rock' Progression)"
     );
+    private final SavedPromptRepository savedPromptRepository;
+    public MusicPromptService(SavedPromptRepository savedPromptRepository){
+        this.savedPromptRepository = savedPromptRepository;
+    }
     public MusicPromptDto getChordProgression(){
         Random random = new Random();
         String chordProgession = chordProgessions.get(random.nextInt(chordProgessions.size()));
         int tempo = ThreadLocalRandom.current().nextInt(60,141);
         String key = "C Major";
         return new MusicPromptDto(key , chordProgession ,tempo);
+    }
+
+    public SavedPrompt savedPrompt(MusicPromptDto promptDto){
+        SavedPrompt newPrompt = new SavedPrompt();
+        newPrompt.setMusicKey(promptDto.getKey());
+        newPrompt.setProgression(promptDto.getProgression());
+        newPrompt.setTempoBPM(promptDto.getTempoBPM());
+        return savedPromptRepository.save(newPrompt);
+    }
+    public List<SavedPrompt> getAllSavedPrompts(){
+        return savedPromptRepository.findAll();
     }
 }
